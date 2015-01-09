9a01a45fe59f529bcc8b9b1d51da49c6b73eb156
mailto.dimitrov@gmail.com <72739091mI>
2015-01-09 05:16:47 +0200

    Final release

---
+++ IntroductionToProgramming/05.PrintYourName/PrintMyName.cs
@@ -0,0 +1,18 @@
+﻿/*
+ Modify the previous application to print your name.
+Ensure you have named the application well (e.g. “PrintMyName”)
+ */
+
+using System;
+
+class PrintMyName
+{
+    static void Main()
+    {
+        Console.WriteLine("My name is pesho!");
+
+        //Console.Write("Enter your name: ");
+        //string userName = Console.ReadLine();
+        //Console.WriteLine("Your name is: {0}", userName);
+    }
+}
---
+++ IntroductionToProgramming/08.SquareRoot/SquareRootOf12345.cs
@@ -0,0 +1,26 @@
+﻿/*
+ Create a console application that calculates and prints the square root of the number 12345.
+Find in Internet “how to calculate square root in C#”.
+ */
+
+using System;
+
+class SquareRootOf12345
+{
+    static void Main()
+    {
+        int number = 12345;
+        double sqrtOfNumber = Math.Sqrt(number);
+
+        Console.WriteLine("The square root of {0} is: {1}", number, sqrtOfNumber);
+        Console.WriteLine("Rounded to integer number: {0}", (int)sqrtOfNumber);
+
+    /*
+    BAD WAY/LESS CODE
+
+        Console.WriteLine("The square root of 12345 is: {0}", Math.Sqrt(12345));
+        Console.WriteLine("Rounded to integer number: {0}", (int)Math.Sqrt(12345));
+    */
+    }
+}
+
