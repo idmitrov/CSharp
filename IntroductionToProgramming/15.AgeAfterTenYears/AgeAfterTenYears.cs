5993e418d81a307948e46fba0032f199e356a4d6
Dimitar <mailto.dimitrov@gmail.com>
2015-01-13 01:53:14 +0200

    Update AgeAfterTenYears.cs

--- IntroductionToProgramming/15.AgeAfterTenYears/AgeAfterTenYears.cs
+++ IntroductionToProgramming/15.AgeAfterTenYears/AgeAfterTenYears.cs
@@ -1,6 +1,4 @@
-/*
- Write a program to read your birthday from the console and print how old you are now and how old you will be after 10 years.
- */
+// Write a program to read your birthday from the console and print how old you are now and how old you will be after 10 years.
 
 using System;
 using System.Globalization;
