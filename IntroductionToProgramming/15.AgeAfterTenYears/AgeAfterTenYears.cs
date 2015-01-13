3727c2c0f564f97e4522da981476d920092cf266
Dimitar <mailto.dimitrov@gmail.com>
2015-01-13 02:27:14 +0200

    Update AgeAfterTenYears.cs

--- IntroductionToProgramming/15.AgeAfterTenYears/AgeAfterTenYears.cs
+++ IntroductionToProgramming/15.AgeAfterTenYears/AgeAfterTenYears.cs
@@ -1,5 +1,4 @@
-// Write a program to read your birthday from the console and print how old you are now and how old you will be after 10 years.
-
+//  Write a program to read your birthday from the console and print how old you are now and how old you will be after 10 years.
 using System;
 using System.Globalization;
 using System.Threading;
@@ -11,19 +10,20 @@ class AgeAfterTenYears
         Console.Write("Type your birthday in format (dd.MM.yyyy): ");
         string userInput = Console.ReadLine(),
                format = "dd.MM.yyyy";
-               
-        try
+        DateTime userBday = default(DateTime);
+
+
+        if (DateTime.TryParseExact(userInput, format, null, DateTimeStyles.None, out userBday))
         {
-            DateTime userBday = DateTime.ParseExact(userInput, format, CultureInfo.InvariantCulture);
             DateTime crntDT = DateTime.Now;
             int age = crntDT.Year - userBday.Year;
 
             Console.WriteLine("Your age: {0}", crntDT.Month >= userBday.Month && crntDT.Day >= userBday.Day ? age : age -= 1);
             Console.WriteLine("Your age in 10 years: {0}", age + 10);
         }
-        catch (Exception)
+        else
         {
-            Console.WriteLine("Error: Invalid date format!);
-        }
+            Console.WriteLine("Error: {0} is not valid DateTime format!", userInput);
+        }   
     }
 }
